bf4f0c4331c324195400f2abc775e8b48c135562
Malte Herold <malte.herold@uni.lu>
2015-04-13 16:28:14 +0200

    added dependencies for gaia

--- conf/gaia_PREPROCESSING_modules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-module load BWA 
-module load SAMtools 
-module load BEDTools
-module load OpenBLAS 
-module load Boost/1.53.0-ictce-5.3.0 
-
-module list
---
+++ modules_to_preload.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l 
+
+
+module load Python
+source env/bin/activate
+
+module load MEGAHIT
+module load BWA 
+module load SAMtools 
+module load BEDTools
+module load OpenBLAS 
+module load Boost/1.53.0-ictce-5.3.0 
+
+export PATH=$PATH:/mnt/nfs/projects/ecosystem_biology/local_tools/idba-1.1.1.icc/bin
+
+module load CAP3
+
+#symbolic links for prokka db 
+module load prokka
+
+export PATH=$PATH:/mnt/nfs/projects/ecosystem_biology/local_tools/tabix-0.2.6
+export PATH=$PATH:/mnt/nfs/projects/ecosystem_biology/local_tools/gkno_launcher/tools/freebayes/bin
+export PATH=$PATH:/mnt/nfs/projects/ecosystem_biology/local_tools/vcftools/bin
+export PERL5LIB=$PERL5LIB:/mnt/nfs/projects/ecosystem_biology/local_tools/vcftools/perl
+export PATH=$PATH:/mnt/nfs/projects/ecosystem_biology/local_tools/Platypus_0.7.9.1
+
+module list
