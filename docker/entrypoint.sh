83730e8c42c2fff57099b90d34aaf0105029278c
Yohan Jarosz <yohanjarosz@yahoo.fr>
2016-05-27 17:21:29 +0200

    fix perm issues?

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-USER_ID=${LOCAL_USER_ID:1000}
-GROUP_ID=${LOCAL_GROUP_ID:1000}
-usermod -u $USER_ID imp
-groupmod -g $GROUP_ID imp
+#usermod -u ${LOCAL_USER_ID} imp
+#groupmod -g ${LOCAL_GROUP_ID} imp
 exec gosu imp "$@"
